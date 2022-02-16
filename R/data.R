247cf3e64ef71f1110f09e18b257aefd42efb195
Ghislain de Laplante <ghislaindel@gmail.com>
2022-02-16 16:02:17 -0700

    With data now!

---
+++ R/data.R
@@ -0,0 +1,28 @@
+#'data.
+#'
+#'@name data
+#'docType package
+NULL
+
+#'Peak SWE data from 1958 to 2021
+#'
+#'Contains only the peak SWE measurement for each year at stations active in 2021.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name ActiveHighSWE
+#' @usage data(ActiveHighSWE)
+#' @format A list of 58 data.frames
+NULL
+
+
+#'Complete snowpack data from 1958 to 2021
+#'
+#'Contains all data at stations active in 2021.
+#'
+#'@docType data
+#'@keywords datasets
+#'@name activeStations
+#'@usage data(activeStations)
+#'@format A list of 58 data.frames
+NULL
